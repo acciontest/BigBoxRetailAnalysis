1506b864bc082da44ad6300671da71eacaaf594b
acciontest <deepak.manoharan@accionlabs.com>
2014-01-06 17:51:36 +0530

    commit

---
+++ BigBoxRetailAnalysis/BigBoxRetailAnalysis.feature
@@ -0,0 +1,17 @@
+﻿Feature: BigBoxRetailAnalysis
+This Analytic App analyzes the networks of three fictitious big box retailers (Box Max, Super Center and Giant Depot).
+
+Background:
+  Given alteryx running at" http://gallery.alteryx.com/"
+  And I am logged in using "deepak.manoharan@accionlabs.com" and "P@ssw0rd"
+  #And I publish the application "twitter tracker"
+  #And I check if the application is "Valid"
+
+
+Scenario Outline:run BigBox Retail App and check for output
+When I run the App "<app>" with <Competitive Trade Area Size (Miles)> and <Maximum Travel Distance Trade Area Size (Miles)>
+Then I see the text in the output "<text>"
+
+Examples: 
+| app                     | Competitive Trade Area Size (Miles) | Maximum Travel Distance Trade Area Size (Miles) | text                                              |
+| Big Box Retail Analysis | 5                                   | 25                                              | Big Box Comparison Report                         |
---
+++ BigBoxRetailAnalysis/BigBoxRetailAnalysisSteps.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using AlteryxGalleryAPIWrapper;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace BigBoxRetailAnalysis
+{
+    [Binding]
+    public class BigBoxRetailAnalysisSteps
+    {
+        private string alteryxurl;
+        private string _sessionid;
+        private string _appid;
+        private string _userid;
+        private string _appName;
+        private string jobid;
+        private string outputid;
+        private string validationId;
+        private string _appActualName;
+        private dynamic statusresp;
+
+        //   private Client Obj = new Client("https://devgallery.alteryx.com/api/");
+        private Client Obj = new Client("https://gallery.alteryx.com/api");
+
+        private RootObject jsString = new RootObject();
+
+
+        [Given(@"alteryx running at""(.*)""")]
+        public void GivenAlteryxRunningAt(string SUT_url)
+        {
+            alteryxurl = Environment.GetEnvironmentVariable(SUT_url);
+        }
+        
+        [Given(@"I am logged in using ""(.*)"" and ""(.*)""")]
+        public void GivenIAmLoggedInUsingAnd(string user, string password)
+        {
+            _sessionid = Obj.Authenticate(user, password).sessionId;
+        }
+        
+        [When(@"I run the App ""(.*)"" with (.*) and (.*)")]
+        public void WhenIRunTheAppWithAnd(string app, int cta, int mtd)
+        {
+            //url + "/apps/gallery/?search=" + appName + "&limit=20&offset=0"
+            //Search for App & Get AppId & userId 
+            string response = Obj.SearchAppsGallery(app);
+            var appresponse =
+                new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Dictionary<string, dynamic>>(
+                    response);
+            int count = appresponse["recordCount"];
+            if (count == 1)
+            {
+                _appid = appresponse["records"][0]["id"];
+                _userid = appresponse["records"][0]["owner"]["id"];
+                _appName = appresponse["records"][0]["primaryApplication"]["fileName"];
+            }
+            else
+            {
+                for (int i = 0; i <= count - 1; i++)
+                {
+
+                    _appActualName = appresponse["records"][i]["primaryApplication"]["metaInfo"]["name"];
+                    if (_appActualName == app)
+                    {
+                        _appid = appresponse["records"][i]["id"];
+                        _userid = appresponse["records"][i]["owner"]["id"];
+                        _appName = appresponse["records"][i]["primaryApplication"]["fileName"];
+                        break;
+                    }
+                }
+
+            }
+            jsString.appPackage.id = _appid;
+            jsString.userId = _userid;
+            jsString.appName = _appName;
+
+            //url +"/apps/" + appPackageId + "/interface/
+            //Get the app interface - not required
+            string appinterface = Obj.GetAppInterface(_appid);
+            dynamic interfaceresp = JsonConvert.DeserializeObject(appinterface);
+
+            //Construct the payload to be posted.
+            List<JsonPayload.Question> questionAnsls = new List<JsonPayload.Question>();
+            questionAnsls.Add(new JsonPayload.Question("Competitive Trade Area Size (Miles)", cta.ToString()));
+            questionAnsls.Add(new JsonPayload.Question("maximum travel distance trade area", mtd.ToString()));
+            jsString.questions.AddRange(questionAnsls);
+            jsString.jobName = "Job Name";
+
+            // Make Call to run app
+
+            var postData = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(jsString);
+            string postdata = postData.ToString();
+            string resjobqueue = Obj.QueueJob(postdata);
+
+            var jobqueue =
+                new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Dictionary<string, dynamic>>(
+                    resjobqueue);
+            jobid = jobqueue["id"];
+            
+            //Get the job status
+            
+            int count1 = 0;
+            string status = "";
+            while (status != "Completed")
+            {
+                System.Threading.Thread.Sleep(500);
+                string jobstatusresp = Obj.GetJobStatus(jobid);
+                var statusResponse =
+                    new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Dictionary<string, dynamic>>(
+                        jobstatusresp);
+                status = statusResponse["status"];
+            }
+
+
+
+        }
+        
+        [Then(@"I see the text in the output ""(.*)""")]
+        public void ThenISeeTheTextInTheOutput(string text)
+        {
+            //url + "/apps/jobs/" + jobId + "/output/"
+            string getmetadata = Obj.GetOutputMetadata(jobid);
+            dynamic metadataresp = JsonConvert.DeserializeObject(getmetadata);
+
+            // outputid = metadataresp[0]["id"];
+            int count = metadataresp.Count;
+            for (int j = 0; j <= count - 1; j++)
+            {
+                outputid = metadataresp[j]["id"];
+            }
+
+            string getjoboutput = Obj.GetJobOutput(jobid, outputid, "html");
+            string htmlresponse = getjoboutput;
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(htmlresponse);
+            string output = doc.DocumentNode.SelectSingleNode("//div[@class='DefaultText']").InnerHtml;
+        //    decimal output1 = Convert.ToDecimal(output);
+          //  decimal finaloutput = Math.Round(output1, 2);
+
+
+            StringAssert.Contains(text, output);
+
+        }
+    }
+}
